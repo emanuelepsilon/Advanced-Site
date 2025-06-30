e6411a3f631c9f54a985b9d149946ddc904ec676
emanuelepsilon <psiho.slesew@gmail.com>
2025-06-30 19:58:16 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -111,11 +111,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
